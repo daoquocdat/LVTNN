b22bd73bf15638f6bc86e8b3f6211b5fa44e7e21
phamdaiminhquan <pdmquan@gmail.com>
2024-12-12 22:56:37 +0700

    lam trang edit

--- src/routes/admin/promotion.route.js
+++ src/routes/admin/promotion.route.js
@@ -1,9 +1,14 @@
 var express = require("express");
 const router = express.Router();
 const promotionController = require('../../app/controllers/admin/promotion.controller.js');
+const { route } = require("./food.route.js");
 
 router.get('/index', promotionController.index);
 router.get('/create', promotionController.create);
 router.post('/store', promotionController.store);
+router.delete('/:id', promotionController.delete);
+router.get('/api/unpromotion/:id', promotionController.getNotPromotionFoods);
+router.get('/api/:id', promotionController.getPromotionFoods);
+router.get('/:id/edit', promotionController.edit);
 
 module.exports = router
